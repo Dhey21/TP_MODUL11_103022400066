3d7b136d1fe5d2e8966da69e8acf5370829343d6
Dhey <dddzul779@gmail.com>
2026-05-10 13:52:12 +0700

    Menambahkan AljabarLibraries

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using AljabarLibraries;
+
+namespace TP_MODUL11_103022400066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] persamaanKuadrat = { 1, -3, -10 };
+            double[] akar = Aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+            Console.WriteLine($"AkarPersamaanKuadrat({{1, -3, -10}})");
+            Console.WriteLine($"Output: {{{akar[0]}, {akar[1]}}}");
+            Console.WriteLine();
+            Console.WriteLine("Hasil Kuadrat ");
+            double[] persamaanLinear = { 2, -3 };
+            double[] hasil = Aljabar.HasilKuadrat(persamaanLinear);
+            Console.WriteLine($"HasilKuadrat({2,-3})");
+            Console.WriteLine($"Output: {hasil[0]}, {hasil[1]}, {hasil[2]}");
+        }
+    }
+}
--- obj/Debug/net10.0/TP_MODUL11_103022400066.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL11_103022400066.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL11_103022400066")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903659a35efa826c6313d66e8bbcc089b84dcea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2cae383d216d2f84d6630fa3b68c25379a39e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL11_103022400066")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL11_103022400066")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
